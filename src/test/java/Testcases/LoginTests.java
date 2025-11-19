b5e9848cd8e62ff8d39e36ba388eae94f524c96e
shital <shitalmotghare2911@gmail.com>
2025-11-20 00:31:27 +0530

    Added the extra lines

--- src/test/java/Testcases/LoginTests.java
+++ src/test/java/Testcases/LoginTests.java
@@ -2,17 +2,35 @@ package Testcases;
 
 import org.testng.annotations.Test;
 
-public class LoginTests 
-{
+public class LoginTests {
+
     @Test
-    public void validLoginTest() 
-    {
+    public void validLoginTest() {
         System.out.println("Running valid login test");
+        System.out.println("Enter username");
+        System.out.println("Enter password");
+        System.out.println("Click login");
+        System.out.println("Validate login success");
     }
-    
+
     @Test
     public void invalidLoginTest() {
         System.out.println("Running invalid login test");
+        System.out.println("Enter wrong username");
+        System.out.println("Enter wrong password");
+        System.out.println("Enter wrong password"); 
+        System.out.println("Enter wrong password"); 
+        System.out.println("Enter wrong password"); 
+        System.out.println("Enter wrong password"); 
+        System.out.println("Enter wrong password"); 
+        System.out.println("Enter wrong password"); 
+        System.out.println("Enter wrong password"); 
+        System.out.println("Enter wrong password"); 
+        System.out.println("Enter wrong password"); 
+        System.out.println("Enter wrong password"); 
+        System.out.println("Enter wrong password"); 
+        System.out.println("Enter wrong password"); 
+        System.out.println("Enter wrong password"); 
+        
     }
 }
-
